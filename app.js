de39bc662766ea2aa90be3f827c63fdab3e8c623
Ridwan Syam <114889236+RidwanSyam@users.noreply.github.com>
2023-04-08 17:50:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Ridwan Syam 52020003\n')
-            ('52020003\n')        
-            (Tehnik Informatika\n')
+    res.send('Ridwan Syam 52020003 Tehnik Informatika\n'))
 })
 
 app.listen(PORT, HOST)
